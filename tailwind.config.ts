71667bb7a9d54ba01b4921d646582dc7404dd8c8
Authtbh <111557161+Authtbh@users.noreply.github.com>
2024-03-16 14:55:26 +0530

    smol changes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -22,7 +22,7 @@ const config: Config = {
         idkfr: "#2a2727",
         idkfrl: "#eed8d1",
         idklol: "#211e1e",
-        meow: "#332626",
+        meow: "#221a1a",
       },
       fontSize: {
         clamp: "clamp(1rem, 6rem, 5rem)",
